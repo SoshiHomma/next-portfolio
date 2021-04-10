4fc7ac98c128e48723beea82088c41754006397e
SoshiHomma <ilovenewtechs@gmail.com>
2021-04-11 07:07:59 +0900

    ?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,65 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 
-import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { motion, useAnimation } from 'framer-motion'
+import { Box, Flex, Grid, Stack, Text } from "@chakra-ui/react"
+import React, { useEffect } from 'react'
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
+const Index = () => {
+  const controls = useAnimation()
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+  useEffect(() => {
+    controls.start(i => ({
+      transform: ["translateY(0px)", "translateY(20px)", "translateY(0px)"],
+      transition: { delay: i * 0.2, ease: "easeInOut", repeat: Infinity, duration: 3, times: [0, 0.5, 1] },
+    }))
+  }, [])
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+  const AnimateText = ({ text, idx }: { text: string, idx: number }) => {
+    return (
+      <motion.p
+        custom={idx}
+        animate={controls}
+      >
+        <Text margin="8px" fontSize={96} fontWeight="bold" color="white">
+          {text}
+        </Text>
+      </motion.p>
+    )
+  }
+
+  return (
+    <Container height="100vh" bgGradient="linear(75deg, rgba(176,61,184,1) 30%, rgba(6,123,167,1) 71%, rgba(0,212,255,1) 100%)">
+      <Flex>
+        {"HI.".split("").map((text, idx) =>
+          <AnimateText key={idx} text={text} idx={idx} />
+        )}
+      </Flex>
+      <Stack justify="center" align="center">
+        <Text fontSize="24" color="white" fontWeight="bold">
+          I am Soshi Homma.
+        </Text>
+        <Text fontSize="24" color="white" fontWeight="bold" background="rgba(255, 255, 255, 0.1)">
+          I develop Technology. System. Anything else.
+        </Text>
+        <Text fontSize="24" color="white" fontWeight="bold" background="rgba(255, 255, 255, 0.1)">
+          I learn hard all the time to do anything.
+        </Text>
+      </Stack>
+      <Stack mt={16}>
+        <Text fontSize="32" color="white" fontWeight="bold" background="rgba(255, 255, 255, 0.2)" p={2}>
+          Works
+        </Text>
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          <Text fontSize="32" color="white" fontWeight="bold" background="rgba(255, 255, 255, 0.2)" p={2}>
+            Gaming site
+        </Text>
+          <Text fontSize="32" color="white" fontWeight="bold" background="rgba(255, 255, 255, 0.2)" p={2}>
+            Another is coming soon...
+        </Text>
+        </Grid>
+      </Stack>
+    </Container>
+  )
+}
 
 export default Index
